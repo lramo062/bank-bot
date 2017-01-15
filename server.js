6363660190b721f02f1f2a595cc2fe23cdee6eee
Lester Ramos <lramo062@fiu.edu>
2017-01-15 14:12:45 -0500

    testing

--- server.js
+++ server.js
@@ -106,39 +106,22 @@ const findOrCreateSession = (fbid) => {
 
 
 const actions = {
-  // send({sessionId}, {text}) {
-  //   // Our bot has something to say!
-  //   // Let's retrieve the Facebook user whose session belongs to
-  //   const recipientId = sessions[sessionId].fbid;
-  //   if (recipientId) {
-  //     // Yay, we found our recipigent!
-  //     // Let's forward our bot response to her.
-  //     // We return a promise to let our bot know when we're done sending
-  //     return sendTextMessage(recipientId, text)
-  //     .then(() => null)
-  //     .catch((err) => {
-  //       console.error(
-  //         'Oops! An error occurred while forwarding the response to',
-  //         recipientId,
-  //         ':',
-  //         err.stack || err
-  //       );
-  //     });
-  //   } else {
-  //     console.error('Oops! Couldn\'t find user for session:', sessionId);
-  //     // Giving the wheel back to our bot
-  //     return Promise.resolve()
-  //   }
-  // },
-  // // You should implement your custom actions here
-    // // See https://wit.ai/docs/quickstart
-
-    send({context, entites}) {
-        return new Promise(function(resolve,reject) {
-            context.name = "Bobby";
-            return resolve(context);
-        });
-    },
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },
+  sayHello({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      // Here should go the api call, e.g.:
+      // context.forecast = apiCall(context.loc)
+      context.name = entities.contact;
+      return resolve(context);
+    });
+  },
 };
 
 
