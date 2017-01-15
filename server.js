8103cf88a96c9a908d1b70f96ea972a46b19c904
Lester Ramos <lramo062@fiu.edu>
2017-01-15 18:03:44 -0500

    fixed error

--- server.js
+++ server.js
@@ -109,9 +109,8 @@ const actions = {
     },
     sayHello({context, entities}) {
         context.name = entities.contact;
-        return Promise.resolve("Hi "
-                               + context.name
-                               + ". I can help you get basic bank information." + 'Please tell me your bank like: "my bank is chase."');
+        let response = 'Hi ' + entities.contact + '. I can help you get basic bank information. Please tell me your bank: "my bank is chase"';
+        return Promise.resolve(response);
     },
 };
 
