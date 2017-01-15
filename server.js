dd21b4572acdd424e49b6dd5cbd489a7ac33ff85
Lester Ramos <lramo062@fiu.edu>
2017-01-15 15:15:36 -0500

    testing

--- server.js
+++ server.js
@@ -10,21 +10,8 @@ const fetch = require('node-fetch');
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const config = require('./config');
 const app = express();
-
-let Wit = null;
-let log = null;
-try {
-  // if running from repo
-  Wit = require('../').Wit;
-  log = require('../').log;
-} catch (e) {
-  Wit = require('node-wit').Wit;
-  log = require('node-wit').log;
-}
-
-const token = "EAADqjcqoQfUBAJO7VdIluMYBQPbVnOHs0L6t1sV8cifJr5isyNpRW9CboAyZCkIo2g7kA2CUPqVvXnvYKDoyBBiZCDm1ZAI5zyMJ2ftH9Y57okvsOA1ZCf2ZAZAYUSRTDt8P5SIqFUpjlmdKVbCnycZBS0NOPGXDBMVNczhJMavrwZDZD";
-
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -42,163 +29,30 @@ app.get('/', function (req, res) {
 
 // for Facebook verification (webhooks)
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'asdfjkl;') {
+    if (req.query['hub.verify_token'] === config.FB_PAGE_TOKEN) {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong token');
 });
 
-// app.post('/webhook/', function (req, res) {
-//     let messaging_events = req.body.entry[0].messaging;
-//     for (let i = 0; i < messaging_events.length; i++) {
-//         let event = req.body.entry[0].messaging[i];
-//         let sender = event.sender.id;
-//         if (event.message && event.message.text) {
-//             let text = event.message.text;
-//             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-//         }
-//     }
-//     res.sendStatus(200);
-// });
-
-
-function sendTextMessage(sender, text) {
-    let messageData = { text:text };
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
-
-
-// This will contain all user sessions.
-// Each session has an entry:
-// sessionId -> {fbid: facebookUserId, context: sessionState}
-const sessions = {};
-
-const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) {
-      // Yep, got it!
-      sessionId = k;
+// to send messages to facebook
+app.post('/webhooks', function (req, res) {
+  var entry = FB.getMessageEntry(req.body)
+  // IS THE ENTRY A VALID MESSAGE?
+  if (entry && entry.message) {
+    if (entry.message.attachments) {
+      // NOT SMART ENOUGH FOR ATTACHMENTS YET
+      FB.newMessage(entry.sender.id, "That's interesting!")
+    } else {
+      // SEND TO BOT FOR PROCESSING
+      Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
+        FB.newMessage(sender, reply)
+      })
     }
-  });
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
   }
-  return sessionId;
-};
 
-
-const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-      console.log('sending...', JSON.stringify(response));
-      return resolve();
-    });
-  },
-  sayHello({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      context.name = entities.contact;
-      return resolve(context);
-    });
-  },
-};
-
-
-// Setting up our bot
-const wit = new Wit({
-  accessToken: '4SEVM5UZWCD3V3QPQJ3HXJCHUFGTHGV5',
-  actions,
-  logger: new log.Logger(log.INFO)
-});
-
-
-
-// Message handler
-app.post('/webhook', (req, res) => {
-  // Parse the Messenger payload
-  // See the Webhook reference
-  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
-  const data = req.body;
-
-  if (data.object === 'page') {
-    data.entry.forEach(entry => {
-      entry.messaging.forEach(event => {
-        if (event.message && !event.message.is_echo) {
-          // Yay! We got a new message!
-          // We retrieve the Facebook user ID of the sender
-          const sender = event.sender.id;
-
-          // We retrieve the user's current session, or create one if it doesn't exist
-          // This is needed for our bot to figure out the conversation history
-          const sessionId = findOrCreateSession(sender);
-
-          // We retrieve the message content
-          const {text, attachments} = event.message;
-
-          if (attachments) {
-            // We received an attachment
-            // Let's reply with an automatic message
-            fbMessage(sender, 'Sorry I can only process text messages for now.')
-            .catch(console.error);
-          } else if (text) {
-            // We received a text message
-
-            // Let's forward the message to the Wit.ai Bot Engine
-            // This will run all actions until our bot has nothing left to do
-            wit.runActions(
-              sessionId, // the user's current session
-              text, // the user's message
-              sessions[sessionId].context // the user's current session state
-            ).then((context) => {
-              // Our bot did everything it has to do.
-              // Now it's waiting for further messages to proceed.
-              console.log('Waiting for next user messages');
-
-              // Based on the session state, you might want to reset the session.
-              // This depends heavily on the business logic of your bot.
-              // Example:
-              // if (context['done']) {
-              //   delete sessions[sessionId];
-              // }
-
-              // Updating the user's current session state
-              sessions[sessionId].context = context;
-            })
-            .catch((err) => {
-              console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
-          }
-        } else {
-          console.log('received event', JSON.stringify(event));
-        }
-      });
-    });
-  }
-  res.sendStatus(200);
-});
-
-
+  res.sendStatus(200)
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
---
+++ wit.js
@@ -0,0 +1,114 @@
+'use strict'
+
+var config = require('./config')
+var FB = require('./facebook')
+var Wit = require('node-wit').Wit
+var request = require('request')
+
+
+var firstEntityValue = function (entities, entity) {
+	var val = entities && entities[entity] &&
+		Array.isArray(entities[entity]) &&
+		entities[entity].length > 0 &&
+		entities[entity][0].value
+
+	if (!val) {
+		return null
+	}
+	return typeof val === 'object' ? val.value : val
+}
+
+
+var actions = {
+	say (sessionId, context, message, cb) {
+		// Bot testing mode, run cb() and return
+		if (require.main === module) {
+			cb()
+			return
+		}
+
+		console.log('WIT WANTS TO TALK TO:', context._fbid_)
+		console.log('WIT HAS SOMETHING TO SAY:', message)
+		console.log('WIT HAS A CONTEXT:', context)
+
+		if (checkURL(message)) {
+			FB.newMessage(context._fbid_, message, true)
+		} else {
+			FB.newMessage(context._fbid_, message)
+		}
+
+		
+		cb()
+		
+	},
+
+	merge(sessionId, context, entities, message, cb) {
+		// Reset the weather story
+		delete context.forecast
+
+		// Retrive the location entity and store it in the context field
+		var loc = firstEntityValue(entities, 'location')
+		if (loc) {
+			context.loc = loc
+		}
+
+		// Reset the cutepics story
+		delete context.pics
+
+		// Retrieve the category
+		var category = firstEntityValue(entities, 'category')
+		if (category) {
+			context.cat = category
+		}
+
+		// Retrieve the sentiment
+		var sentiment = firstEntityValue(entities, 'sentiment')
+		if (sentiment) {
+			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
+		} else {
+			delete context.ack
+		}
+
+		cb(context)
+	},
+
+	error(sessionId, context, error) {
+		console.log(error.message)
+	},
+
+	// list of functions Wit.ai can execute
+	['fetch-weather'](sessionId, context, cb) {
+		// Here we can place an API call to a weather service
+		// if (context.loc) {
+		// 	getWeather(context.loc)
+		// 		.then(function (forecast) {
+		// 			context.forecast = forecast || 'sunny'
+		// 		})
+		// 		.catch(function (err) {
+		// 			console.log(err)
+		// 		})
+		// }
+
+		context.forecast = 'Sunny'
+
+		cb(context)
+	},
+
+	['fetch-pics'](sessionId, context, cb) {
+		var wantedPics = allPics[context.cat || 'default']
+		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
+
+		cb(context)
+	},
+}
+
+// SETUP THE WIT.AI SERVICE
+var getWit = function () {
+	console.log('GRABBING WIT')
+	return new Wit(config.WIT_TOKEN, actions)
+}
+
+module.exports = {
+	getWit: getWit,
+}
+
