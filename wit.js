4992121f1e008ae48a2eb69a10e5c7ed07a3e833
Lester Ramos <lramo062@fiu.edu>
2017-01-15 15:36:33 -0500

    test

--- wit.js
+++ wit.js
@@ -21,7 +21,34 @@ var firstEntityValue = function (entities, entity) {
 
 
 var actions = {
-	say (sessionId, context, message, cb) {
+    send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+
+
+
+    say (sessionId, context, message, cb) {
 		// Bot testing mode, run cb() and return
 		if (require.main === module) {
 			cb()
